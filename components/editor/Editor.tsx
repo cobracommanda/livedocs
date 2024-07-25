457440b970d6894919ed6fab45c87a47bba24e30
DRobinson <drobinson@benchmarkeducation.com>
2024-07-24 22:14:03 -0400

    add notification

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -20,6 +20,7 @@ import Loader from "../Loader";
 import FloatingToolbarPlugin from "./plugins/FloatingToolbar";
 import { useThreads } from "@liveblocks/react/suspense";
 import Comments from "../Comments";
+import { DeleteModal } from "../DeleteModal";
 // Catch any errors that occur during Lexical updates and log them
 // or throw them as needed. If you don't throw them, Lexical will
 // try to recover gracefully without losing user data.
@@ -53,7 +54,7 @@ export function Editor({
       <div className="editor-container size-full">
         <div className="toolbar-wrapper flex min-w-full justify-between">
           <ToolbarPlugin />
-          {/* {currentUserType === 'editor' && <DeleteModal roomId={roomId}/>} */}
+          {currentUserType === "editor" && <DeleteModal roomId={roomId} />}
         </div>
 
         <div className="editor-wrapper flex flex-col items-center justify-start">
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,11 +1,14 @@
 "use server";
 
-import { clerkClient } from "@clerk/nextjs/server";
+import { clerkClient, currentUser } from "@clerk/nextjs/server";
 import { parseStringify } from "../utils";
+import { liveblocks } from "../liveblocks";
 
 export const getClerkUsers = async ({ userIds }: { userIds: string[] }) => {
   try {
-    const { data } = await clerkClient.users.getUserList({
+    // Updated way to use clerkClient as a function
+    const clerk = clerkClient();
+    const { data } = await clerk.users.getUserList({
       emailAddress: userIds,
     });
 
@@ -23,5 +26,34 @@ export const getClerkUsers = async ({ userIds }: { userIds: string[] }) => {
     return parseStringify(sortedUsers);
   } catch (error) {
     console.log(`Error fetching users: ${error}`);
+  }
+};
+
+export const getDocumentUsers = async ({
+  roomId,
+  currentUser,
+  text,
+}: {
+  roomId: string;
+  currentUser: string;
+  text: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+    const users = Object.keys(room.usersAccesses).filter(
+      (email) => email !== currentUser
+    );
+
+    if (text.length) {
+      const lowerCaseText = text.toLowerCase();
+
+      const filteredUsers = users.filter((email: string) =>
+        email.toLowerCase().includes(lowerCaseText)
+      );
+      return parseStringify(filteredUsers);
+    }
+    return parseStringify(users);
+  } catch (error) {
+    console.log(`Error fetching documents users: ${error}`);
   }
 };
